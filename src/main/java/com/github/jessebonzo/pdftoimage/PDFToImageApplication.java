fadda5d6b4b162d34fe9bb43f9a92072bb35b0c2
Jesse Bonzo <j.bonzo@gmail.com>
2022-05-14 14:20:31 -0700

    Added logo

--- src/main/java/com/github/jessebonzo/pdftoimage/PDFToImageApplication.java
+++ src/main/java/com/github/jessebonzo/pdftoimage/PDFToImageApplication.java
@@ -3,6 +3,7 @@ package com.github.jessebonzo.pdftoimage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
@@ -11,6 +12,9 @@ import java.io.IOException;
 public class PDFToImageApplication extends Application {
   @Override
   public void start(Stage primaryStage) throws IOException {
+    primaryStage
+        .getIcons()
+        .add(new Image(PDFToImageApplication.class.getResourceAsStream("logo.png")));
     FXMLLoader fxmlLoader =
         new FXMLLoader(PDFToImageApplication.class.getResource("main-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 800, 600);
