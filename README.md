a14f35b7ac77f3914979e7e3b163808c1e465fe1
Jesse Bonzo <j.bonzo@gmail.com>
2022-05-14 17:52:44 -0700

    Create executable jar

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PDFToImage
+
+A simple program to convert a PDF to one PNG.
+
+## Building
+
+`.\mvnw package`
+
+## Running
+
+`java -jar target\PDFToImage-1.0-SNAPSHOT-shaded.jar`
---
+++ src/main/java/com/github/jessebonzo/pdftoimage/ApplicationStarter.java
@@ -0,0 +1,7 @@
+package com.github.jessebonzo.pdftoimage;
+
+public class ApplicationStarter {
+  public static void main(String[] args) {
+    PDFToImageApplication.main(args);
+  }
+}
